fdc290f066f3f08b3c5c132139d383e44488c8c0
Abdullah <ab-benkathir@outlook.com>
2021-09-07 11:21:44 +0200

    bestanden

---
+++ feestlunch.py
@@ -0,0 +1,10 @@
+croissantjes = 0.39
+Aantalcroissantjes = 17
+Aantalstokbroden = 2
+stokbroden  =  2.78
+Aantalkortingsbonnen = 3
+kortingsbonnen = 0.50
+
+print(Aantalcroissantjes * croissantjes + Aantalstokbroden * stokbroden - Aantalkortingsbonnen * kortingsbonnen)
+
+print("De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!")
---
+++ speelhaldag.py
@@ -0,0 +1,8 @@
+vrienden = 3
+toegangsticket  = 7.45
+VIPVRGameSea = 0.37
+minuten = 9
+
+print(vrienden * toegangsticket + VIPVRGameSea * minuten * vrienden )
+
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 32.34 euro")
